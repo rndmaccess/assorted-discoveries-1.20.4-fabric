994307e49e033f1b0795575b32b3ca0fa12ba6ca
rndmaccess <rndm.access.dev@gmail.com>
2024-03-03 12:18:15 -0700

    All container items now add an empty container to the player's inventory if the food/drink is stacked otherwise the empty container is returned in the player's hand.

--- src/main/java/rndm_access/assorteddiscoveries/item/ADFoodContainerItem.java
+++ src/main/java/rndm_access/assorteddiscoveries/item/ADFoodContainerItem.java
@@ -1,32 +1,14 @@
 package rndm_access.assorteddiscoveries.item;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.world.World;
 
-public class ADFoodContainerItem extends Item {
-    private final Item returnItem;
-
+public class ADFoodContainerItem extends ADContainerItem {
     public ADFoodContainerItem(Item.Settings settings) {
-        super(settings);
-        this.returnItem = Items.BOWL;
+        super(settings, Items.BOWL);
     }
 
-    public ADFoodContainerItem(Item.Settings settings, Item returnItem) {
-        super(settings);
-        this.returnItem = returnItem;
-    }
-
-    @Override
-    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        user.eatFood(world, stack);
-
-        if(user instanceof PlayerEntity player && player.isCreative()) {
-            return stack;
-        }
-        return new ItemStack(this.returnItem);
+    public ADFoodContainerItem(Item.Settings settings, Item emptyContainer) {
+        super(settings, emptyContainer);
     }
 }
